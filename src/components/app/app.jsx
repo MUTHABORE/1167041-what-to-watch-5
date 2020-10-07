60ff5496fe9cd2797e7e68c98bf520daf7e9c9c4
muthabore <xansex@rambler.ru>
2020-10-07 23:48:47 +0300

    save

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,10 +1,38 @@
 import React from 'react';
-import MainPage from '../main-page/main-page.jsx';
 import PropTypes from 'prop-types';
+import {Switch, Route, BrowserRouter} from 'react-router-dom';
 
-const App = (props) => {
+import Main from '../main/main.jsx';
+import SignIn from '../sign-in/sign-in.jsx';
+import MyList from '../my-list/my-list.jsx';
+import Film from '../film/film.jsx';
+import AddReview from '../add-review/add-review.jsx';
+import Player from '../player/player.jsx';
+
+const App = ({film}) => {
   return (
-    <MainPage film={props.film}/>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact>
+          <Main film={film}/>
+        </Route>
+        <Route path="/login" exact>
+          <SignIn />
+        </Route>
+        <Route path="/mylist" exact>
+          <MyList />
+        </Route>
+        <Route path="/films/:id" exact>
+          <Film />
+        </Route>
+        <Route path="/films/:id/review" exact>
+          <AddReview />
+        </Route>
+        <Route path="/player/:id" exact>
+          <Player />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
